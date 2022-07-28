c9fff8ac1ffd46c681cecb594115bd6c23ffb366
Blaise Thompson <blaise@untzag.com>
2022-07-28 14:12:23 -0500

    mypy

--- hart_protocol/_parsing.py
+++ hart_protocol/_parsing.py
@@ -1,35 +1,34 @@
 import struct
+from typing import MutableMapping, Union
 
 
-
-
-def parse(response: bytes) -> dict:
-    out = dict()
-    out["full_response"]: bytes = response
+def parse(response: bytes) -> MutableMapping[str, Union[int, bytes, str, float]]:
+    out: MutableMapping[str, Union[int, bytes, str, float]] = dict()
+    out["full_response"] = response
     if response[0] & 0x80:  # long address
-        out["address"]: bytes = response[1:6]
+        out["address"] = int.from_bytes(response[1:6], "big")
         response = response[6:]
     else:  # short address
-        out["address"]: bytes = response[1]
+        out["address"] = response[1]
         response = response[2:]
     command, bytecount, status = struct.unpack_from(">BBL", response)
     out["status"] = status
-    data = response[4:4+bytecount]
-    out["command"]: int = command
-    out["command_name"]: str = f"hart_command_{command}"
-    out["bytecount"]: int = bytecount
-    out["data"]: bytes = data
+    data = response[4 : 4 + bytecount]
+    out["command"] = command
+    out["command_name"] = f"hart_command_{command}"
+    out["bytecount"] = bytecount
+    out["data"] = data
     if command in [0, 11]:
         out["command_name"] = "read_unique_identifier"
-        out["manufacturer_id"]: bytes = data[1]
-        out["manufacturer_device_type"]: bytes = data[2]
-        out["number_response_preamble_characters"]: bytes = data[3]
-        out["universal_command_revision_level"]: bytes = data[4]
-        out["transmitter_specific_command_revision_level"]: bytes = data[5]
-        out["software_revision_level"]: bytes = data[6]
-        out["hardware_revision_level"]: bytes = data[7]
-        out["device_id"]: bytes = data[9:12]
+        out["manufacturer_id"] = data[1]
+        out["manufacturer_device_type"] = data[2]
+        out["number_response_preamble_characters"] = data[3]
+        out["universal_command_revision_level"] = data[4]
+        out["transmitter_specific_command_revision_level"] = data[5]
+        out["software_revision_level"] = data[6]
+        out["hardware_revision_level"] = data[7]
+        out["device_id"] = data[9:12]
     if command in [1]:
         out["command_name"] = "read_primary_variable"
-        out["primary_variable"] = struct.unpack_from(">f", data)
+        out["primary_variable"] = struct.unpack_from(">f", data)[0]
     return out
--- hart_protocol/universal.py
+++ hart_protocol/universal.py
@@ -4,41 +4,52 @@ from . import tools
 def read_unique_identifier(address: bytes) -> bytes:
     return tools.pack_command(address, command_id=0)
 
+
 def read_primary_variable(address: bytes) -> bytes:
     return tools.pack_command(address, command_id=1)
+
 
 def read_loop_current_and_percent(address: bytes) -> bytes:
     return tools.pack_command(address, command_id=2)
 
+
 def read_dynamic_variables_and_loop_current(address: bytes) -> bytes:
     return tools.pack_command(address, command_id=3)
+
 
 def write_polling_address(address: bytes, new_polling_address: int) -> bytes:
     assert 0 <= new_polling_address <= 15
     return tools.pack_command(address, command_id=6, data=new_polling_address.to_bytes(1, "big"))
 
+
 def read_unique_identifier_associated_with_tag(tag: bytes, *, address: int = 0) -> bytes:
     return tools.pack_command(address, command_id=11, data=tag)
+
 
 def read_message(address: bytes) -> bytes:
     return tools.pack_command(address, command_id=12)
 
+
 def read_tag_descriptor_date(address: bytes) -> bytes:
     return tools.pack_command(address, command_id=13)
+
 
 def read_primary_variable_information(address: bytes) -> bytes:
     return tools.pack_command(address, command_id=14)
 
+
 def read_output_information(address: bytes) -> bytes:
     return tools.pack_command(address, command_id=15)
+
 
 def read_final_assembly_number(address: bytes) -> bytes:
     return tools.pack_command(address, command_id=16)
 
+
 def write_message(address: bytes, message: str) -> bytes:
     message = message.ljust(32)
-    message = tools.pack_ascii(message)
-    return tools.pack_command(address, command_id=17, data=message)
+    return tools.pack_command(address, command_id=17, data=tools.pack_ascii(message))
+
 
 def write_tag_descriptor_date(address: bytes, tag: str, descriptor: str, date: tuple):
     data = b""
@@ -51,6 +62,7 @@ def write_tag_descriptor_date(address: bytes, tag: str, descriptor: str, date: t
     data += month.to_bytes(1, "big")
     data += year.to_bytes(1, "big")
     return tools.pack_command(address, command_id=18, data=data)
+
 
 def write_final_assembly_number(address: bytes, number: int):
     data = number.to_bytes(3, "big")
